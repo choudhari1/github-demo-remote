8de1075020269e06777f915b044e05b7b64760e0
choudhari1 <57314380+choudhari1@users.noreply.github.com>
2021-09-03 08:29:05 +0530

    initial commit for math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+add implementaion
+def add(a,b):
+	return x+y
+sub implementaion
+def subs(a,b):
+	return a-b //on master
+mult implementaion
+def mult(a,b):
+	return a*b //bug456
+div implementaion
+def divide(a,b):
+	if b==0:
+		return 'cant divide by zero'
+	else:
+		return a/b
+added square fn
+def square(x):
+	return x*x
+
