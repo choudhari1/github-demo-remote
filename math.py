b225c78f4c7aeabf5c24284322f66c1367d9b23e
Pavan_Choudhari <pavanchoudhari071@gmail.com>
2021-09-03 08:35:14 +0530

    added fn method

--- math.py
+++ math.py
@@ -1,13 +1,17 @@
 add implementaion
+added add fn
 def add(a,b):
 	return x+y
 sub implementaion
+added sub fn
 def subs(a,b):
 	return a-b //on master
 mult implementaion
+added mult fn
 def mult(a,b):
 	return a*b //bug456
 div implementaion
+added div fn
 def divide(a,b):
 	if b==0:
 		return 'cant divide by zero'
